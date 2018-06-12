f236e761e154e022f3de78bf85bc9d13271c6e01
peterkarpov <peter_karpov@mail.ru>
2018-06-12 14:08:41 +0400

    development

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  }
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+
 };
