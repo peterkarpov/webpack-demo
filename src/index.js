572c794465dc15e0d26cce1613a6f831f4734df3
peterkarpov <peter_karpov@mail.ru>
2018-06-12 14:01:58 +0400

    asset-managment-1

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
 
 function component() {
-    var element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+        var element = document.createElement('div');
 
-  window.addEventListener('load', function(){
-     
-          alert(e.target);
-     
-  });
+        // Lodash, now imported by this script
+        element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+        element.classList.add('hello');
+
+        // Add the image to our existing div.
+        var myIcon = new Image();
+        myIcon.src = Icon;
+        myIcon.name = '136.png';
+
+        element.appendChild(myIcon);
+
+        
+
+        return element;
+}
+
+document.body.appendChild(component());
+
+window.addEventListener('load', function () {
+
+        console.log('start');
+
+});
+
+console.log(Data);
+
+
